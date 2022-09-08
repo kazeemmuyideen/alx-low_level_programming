74c0b45e62260a6be01e8c224c69e43be897d985
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-09 00:09:37 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * File: 5-printf.c
+ * Auth: Muyideen <kazeemmuyideen@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
+	return (0);
+}
