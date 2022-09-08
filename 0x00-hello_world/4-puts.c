647499c68eca2ffd108428a04e065a858388be73
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-08 23:48:31 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Muyideen
+ * 
+ */
+
 #include<stdio.h>
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+
+/**
+ * main - Entry point
+ * Description: This program is about using put to output on stdio
+ * Return: Always 0 
+ */
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+	}
