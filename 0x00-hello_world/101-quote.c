5af2b171638fce970b2c7d1a6572d4cf0f8909bc
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-09 13:11:01 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,7 @@
 */
 int main(void)
 {
-	int n;
-
-	n = write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 80);
-	printf("value of n is %d", n);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 80);
 	return (0);
 }
 
