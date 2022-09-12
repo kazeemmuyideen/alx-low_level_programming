66b1922d9f309095a8a2c9d5f62291e9eb979910
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-12 04:49:36 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this program prints alphabet in lower case followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+		putchar("\n")
+	}
+
+	return (0);
+}
