2e4d6d85241978d488a238db6d43cc86e4d72927
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-14 21:30:44 +0100

    10-main and 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - This function add two number together
+ *
+ * @num1: This is parameter
+ *
+ * @num2: This is a parameter
+ *
+ * Return: Always 0 (success)
+ **/
+
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
