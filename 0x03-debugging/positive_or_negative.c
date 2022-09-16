a9300a17f8097eaaee0f73f6205e96d62d649885
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-16 04:26:02 +0100

    Debuging some code

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * positive_or_negative - Prints a random number and states either
+ * it is positive, negative, or zero.
+ * @n: This is interger
+ * Return: Always 0.
+ */
+
+int positive_or_negative(int n)
+{
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n <  0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
