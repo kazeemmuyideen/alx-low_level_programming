f6828fa65f11ba106f7108e0c7dfca40df3750ff
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-20 06:52:15 -0700

    Done with task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - A function that swaps the value of two intergers
+ *
+ * @a: It's a pointer a
+ *
+ * @b: It's a pointer b
+ *
+ * Return: Always 0
+ **/
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
