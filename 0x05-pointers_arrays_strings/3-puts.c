0f5f169ea5466eca3182e4a5af5660e1c020d6f1
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-20 07:57:50 -0700

    Done with task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _puts - This function print string follow by new line
+ *
+ * @str: The parameter
+ *
+ *Return: Always 0
+ **/
+void _puts(char *str)
+{
+	puts(str);
+}
