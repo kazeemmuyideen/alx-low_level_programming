076ba1b97383cb3890ac184731718f96025c7b0a
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-23 14:52:12 +0100

    Done with task 03

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - A function that concatenate 2 strings
+ *
+ * @s1: Variable 1 that accept string
+ *
+ * @s2: Variable 2 that accept strings
+ *
+ * Return: Always 0
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
