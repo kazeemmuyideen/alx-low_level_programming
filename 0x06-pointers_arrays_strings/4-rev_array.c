0a41682cc6b2e5658306b785ae144eea7e14678c
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-23 15:40:09 +0100

    Done with task 04

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - A function that array in reverse form
+ *
+ * @a: Variable 1 that accept integer
+ *
+ * @n: number of element in the array
+ *
+ * Return: Always 0
+ **/
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = n-1; i >= n/2; i--)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = tmp;
+	}
+
+}
