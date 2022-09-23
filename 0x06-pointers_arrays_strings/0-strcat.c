a8116770187c03acb78a38e05794c8b308580bc5
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-23 12:33:23 +0100

    Starting the 0x06-pointes_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - A function that concatenate 2 strings
+ *
+ * @dest: Variable 1 that accept string
+ *
+ * @src: Variable 2 that accept strings
+ *
+ * Return: Always 0
+ **/
+
+char *_strcat(char *dest, char *src)
+{
+
+	return (strcat(dest, src));
+}
