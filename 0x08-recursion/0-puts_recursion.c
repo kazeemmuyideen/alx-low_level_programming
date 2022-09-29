8b611324623c37c63df79af9758b31cd193cabb8
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-29 05:26:09 +0100

    Starting 0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts_recursion - recursion function that returns string
+*
+* @s:  parameter
+*/
+void _puts_recursion(char *s)
+{
+	puts(s);
+	_putchar('\n');
+}
