68d4a5c381f7346fcf4e2a1ac5ebded4b3e09004
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-09-29 23:21:24 +0100

    Done with the task 1 & 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,18 @@
 *_strlen_recursion - recursion function that returns length
 *
 * @s:  parameter
+*
+*Return: length
 */
 int _strlen_recursion(char *s)
 {
 	int length;
 
-	if (*s == '\0')
-		return (0);
-	else
+	if (*s != '\0')
+	{
 		length = 1 + _strlen_recursion(s + 1);
-
-	return (length);
+		return (length);
+	}
+	else
+		return (0);
 }
