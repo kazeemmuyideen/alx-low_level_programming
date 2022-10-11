25dabe7d41f987270e62e8e6d36b57bd176fdb54
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-10-11 13:02:51 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a new type
+ *
+ * @name: the name of the dog (Bingo)
+ * @age: the age of the dog ()
+ * @owner: the name of the owner (Muyi)
+ */
+
+typedef struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
+	dog;
+
+#endif /*dog.h*/
+
