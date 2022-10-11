b917c05d3201d856813008b37dee483c9bf20317
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-10-11 15:14:27 +0100

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,7 @@
  * @name: the name of the dog (Bingo)
  * @age: the age of the dog ()
  * @owner: the name of the owner (Muyi)
+ * @print_dog: Prints dog infor
  */
 
 typedef struct dog
@@ -15,14 +16,24 @@ typedef struct dog
 	float age;
 	char *owner;
 }
-	typedef struct dog dog_t; /*3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read*/
+	typedef struct dog dog_t; /** 3. Outside of a dog, a book is a man's
+				   * best friend. Inside of a dog it's
+				   * too dark to read
+				   **/
+
 
 	dog; /*0. Poppy */
 
-	void init_dog(struct dog *d, char *name, float age, char *owner); /* 1. A dog is the only thing on earth that loves you more than you love yourself */
-	
-	void print_dog(struct dog *d); /*2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad */
-	
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	/**
+	 * 1. A dog is the only thing on earth
+	 * that loves you more than you love yourself
+	 **/
+
+	void print_dog(struct dog *d); /** 2. A dog will teach you unconditional love.
+					* If you can have that in your life,
+					* things won't be too bad
+					**/
 
 #endif /*dog.h*/
 
