7f2f8e981c433f5df1d372189f2de8f4baad6e4f
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-10-12 12:30:54 +0100

    task 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - this function prints the file name of the program
+ *
+ * Return: Always 0
+ **/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n ", __FILE__);
+
+	return (0);
+}
