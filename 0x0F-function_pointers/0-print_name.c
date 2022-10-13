3635385395639b881e8d3c5ef79d48f36bdc7229
kazeemmuyideen <kazeemmuyideen@gmail.com>
2022-10-13 09:01:03 +0100

    0x0F-function_pointers 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ *
+ * @name: pointer to a char
+ * @f: a pointer to function that receives a pointer to char
+ *
+ * Return: void or NULL
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	/* Condition for the two pointers they must exist*/
+	/* also could be if they are different to null*/
+	if (name && f)
+	{
+		/* derefrence the pointer to the function f*/
+		/* to the pointer name */
+		/* f receive a char *, and *name is type char */
+		f(name);
+		
+	}
+}
